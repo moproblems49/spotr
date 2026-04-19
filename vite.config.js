72255a33edc24050e934713c39e10d7bfd4bbb1b
moproblems49 <Haggag.a.Mohammed@gmail.com>
2026-04-19 17:57:53 -0400

    Disable CSS minification to fix lightningcss build crash

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssMinify: 'esbuild',
-    minify: 'esbuild',
+    cssMinify: false,
   },
 })
